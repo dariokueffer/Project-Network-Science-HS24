c0baa3e8a1823d8bc2758fea58a5e6db2d5767b6
Dario Küffer <dariokuffer@LaptopvonDario.net>
2024-12-30 14:24:30 +0100

    fixed plot

--- src/analysis/utils.py
+++ src/analysis/utils.py
@@ -1,5 +1,6 @@
 from graph_tool.generation import random_rewire
 import matplotlib.pyplot as plt
+import pandas as pd
 
 def randomize_graph(G):
     randomized_graph = G.copy()
@@ -43,3 +44,51 @@ def plot_stats_for_category_quarterly(df, category_name):
 
     plt.tight_layout()
     plt.show()
+
+def plot_stats_for_category_quarterly(df, category_name):
+        if not pd.api.types.is_datetime64_any_dtype(df['month']):
+                df['month'] = pd.to_datetime(df['month'])
+
+        df['quarter'] = df['month'].dt.to_period('Q')
+
+        min_quarter = df['quarter'].min()
+        max_quarter = df['quarter'].max()
+        all_quarters = pd.period_range(start=min_quarter, end=max_quarter, freq='Q')
+
+        complete_quarters_df = pd.DataFrame(index=all_quarters)
+        complete_quarters_df.index.name = 'quarter'
+
+        quarterly_df = (
+                df.groupby('quarter')[['total_contributions', 'unique_pages', 'unique_contributors']]
+                .sum()
+        )
+
+        quarterly_df = (
+                complete_quarters_df
+                .join(quarterly_df)
+                .fillna(0)
+                .reset_index()
+        )
+
+        quarterly_df['quarter'] = quarterly_df['quarter'].astype(str)
+
+        plt.figure(figsize=(10, 6))
+        plt.plot(quarterly_df['quarter'], quarterly_df['unique_pages'], 
+                label='Unique Pages')
+        plt.plot(quarterly_df['quarter'], quarterly_df['unique_contributors'], 
+                label='Unique Contributors')
+        plt.plot(quarterly_df['quarter'], quarterly_df['total_contributions'], 
+                label='Total Contributions')
+
+        plt.title(f'Quarterly Statistics for Main Category: {category_name}')
+        plt.xlabel('Quarter')
+        plt.ylabel('Count')
+        plt.legend()
+        plt.grid(True)
+
+        xticks = quarterly_df['quarter'][::20]  
+        plt.xticks(ticks=range(0, len(quarterly_df['quarter']), 20), 
+                labels=xticks, rotation=45)
+
+        plt.tight_layout()
+        plt.show()
